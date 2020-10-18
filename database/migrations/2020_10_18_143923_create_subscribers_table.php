6db0e8f8efdf6242c96e6fd7e68c78a671bd7699
Furkat Subkhonkulov <subhonkulov2@gmail.com>
2020-10-18 21:31:35 +0500

    init commit

---
+++ database/migrations/2020_10_18_143923_create_subscribers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscribersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable(false);
+            $table->string('last_name')->nullable(false);
+            $table->string('email')->nullable(false);
+            $table->boolean('mailing_status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscribers');
+    }
+}
---
+++ database/seeders/SubscribersSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Subscriber;
+use Carbon\Carbon;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class SubscribersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $fakeSubs = $this->makeFakeSubscribers();
+        Subscriber::query()->insert($fakeSubs);
+    }
+
+    private function makeFakeSubscribers() {
+        $fakeSubs = [];
+        $faker = Factory::create('ru');
+        for ($i = 0; $i <= 10; $i++) {
+            $fakeSubs[] = [
+                'first_name' => $faker->name,
+                'last_name' => $faker->lastName,
+                'email' => $faker->email,
+                'mailing_status' => false, // due to the task request
+                'created_at' => Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon::now()->toDateTimeString()
+            ];
+        }
+        return $fakeSubs;
+    }
+}
